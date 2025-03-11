10b6227ef491131e4cb270ee6e685d4abc0f3d57
PRAKTIKAN <PRAKTIKAN@DESKTOP-MU6PA4D>
2025-03-11 08:14:06 +0700

    menambahkan class fanlaptop

--- Main.cs
+++ Main.cs
@@ -1,19 +1,48 @@
 ﻿using System;
 
+//namespace jurnalmodul4_103022330122
+//{
+//    class kodeproduk
+//    {
+//        static void Main()
+//        {
+//            KodeProduk kodeproduk = new KodeProduk();
+
+//            Console.WriteLine("Input Produk Elektronik: ");
+//            String Produk = Console.ReadLine();
+//            String KodeProduk = kodeproduk.GetKodeProduk(Produk);    
+//            Console.WriteLine($"{Produk}: {kodeproduk.GetKodeProduk(Produk)}");
+
+//        }
+//    } 
+//}
+
 namespace jurnalmodul4_103022330122
 {
-    class kodeproduk
+    class Mfanlaptop
     {
         static void Main()
         {
-            KodeProduk kodeproduk = new KodeProduk();
+            FanLaptop fan = new FanLaptop();
 
-            Console.WriteLine("Input Produk Elektronik: ");
-            String Produk = Console.ReadLine();
-            String KodeProduk = kodeproduk.GetKodeProduk(Produk);    
-            Console.WriteLine($"{Produk}: {kodeproduk.GetKodeProduk(Produk)}");
-           
+            while (true)
+            {
+                Console.WriteLine("\nMasukkan perintah (Balanced, Performance, Turbo): ");
+                String input = Console.ReadLine().ToLower();
+
+                if (input == "Balanced")
+                {
+                    fan.Balanced();
+                }
+                else if (input == "Performance")
+                {
+                    fan.Performance();
+                }
+                else if (input == "Turbo")
+                {
+                    fan.Turbo();
+                }
+            }
         }
-    } 
+    }
 }
-
--- Program.cs
+++ Program.cs
@@ -4,32 +4,51 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//namespace jurnalmodul4_103022330122
+//{
+
+//    class KodeProduk
+//    {
+//        public static Dictionary<string, string> TKodeproduk = new Dictionary<string, string>
+//    {
+//            {"Laptop","E100"},
+//            {"Smartphone","E101"},
+//            {"Tablet","E102"},
+//            {"Headset","E103"},
+//            {"Keyoard","E104"},
+//            {"Mouse","E105"},
+//            {"Printer","E106"},
+//            {"Monitor","E107"},
+//            {"Smartwatch","E108"},
+//            {"Kamera","E109"}
+//    };
+//        public string GetKodeProduk(string produk)
+//        {
+//               if (TKodeproduk.ContainsKey(produk))
+//            {
+//                return TKodeproduk[produk];
+//            }
+//            return "Produk tidak ada";
+//        }
+
+//    }
+//}
+
 namespace jurnalmodul4_103022330122
 {
+    class FanLaptop
+    {
+        private enum State { Balanced, Quiet, Performance, Turbo }
+        private State currentState = State.Quiet;
+        
 
-    class KodeProduk
-    {
-        public static Dictionary<string, string> TKodeproduk = new Dictionary<string, string>
-    {
-            {"Laptop","E100"},
-            {"Smartphone","E101"},
-            {"Tablet","E102"},
-            {"Headset","E103"},
-            {"Keyoard","E104"},
-            {"Mouse","E105"},
-            {"Printer","E106"},
-            {"Monitor","E107"},
-            {"Smartwatch","E108"},
-            {"Kamera","E109"}
-    };
-        public string GetKodeProduk(string produk)
+        switch(fan)
         {
-               if (TKodeproduk.ContainsKey(produk))
-            {
-                return TKodeproduk[produk];
-            }
-            return "Produk tidak ada";
-        }
-
+            case fan.Balanced:
+            if( == "ModeUp")
+            state = fan.Performance;
+         }
     }
 }
+
+        
